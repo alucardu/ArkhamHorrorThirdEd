28edabd348822f44efca6b0a5a903e12df0e9f62
Peter Boomsma <peter@cleanpixel.nl>
2021-05-21 14:39:30 +0200

    fixed bugs with headlines

--- readHeadlines.lua
+++ readHeadlines.lua
@@ -1,32 +1,33 @@
 function readHeadline()
   headlinesDeck = getObjectFromGUID('3e1179').getVar('headlinesDeck')
-  headlinesDeckPos = headlinesDeck.getPosition()
+  if headlinesDeck ~= null then headlinesDeckPos = headlinesDeck.getPosition() end
 
-  if getObjectFromGUID('3e1179').getVar('headlinesDeck') == null then
+  if headlinesDeck == null then
     if lastCard == nill then
       broadcastToAll("Add Doom", {1,0,0})
       getObjectFromGUID('f807c7').takeObject({
-        position={x = headlinesDeckPos.x + 3, y = headlinesDeckPos.y, z = headlinesDeckPos.z }
+        position={x = headlinesDeckPos.x + 3, y = headlinesDeckPos.y + 5, z = headlinesDeckPos.z }
       })
       return
     end
     lastCard.setPositionSmooth(
-      {x = headlinesDeckPos.x + 3, y = headlinesDeckPos.y, z = headlinesDeckPos.z }
+      {x = headlinesDeckPos.x + 3, y = headlinesDeckPos.y + 5, z = headlinesDeckPos.z }
     )
-
-    local flipCard = function() lastCard.flip() lastCard = nill end
-    moveWatch = function() return not lastCard.isSmoothMoving() end
-    Wait.condition(flipCard, moveWatch)
+    Wait.condition(
+      function() lastCard.setRotationSmooth({0, 180, 0}) end,
+      || not lastCard.isSmoothMoving()
+    )
     return
   end
 
   if headlinesDeck.remainder == nill then
     headlineCard = headlinesDeck.takeObject({
-      position={x = headlinesDeckPos.x + 3, y = headlinesDeckPos.y, z = headlinesDeckPos.z},
-      callback_function = function()
-        headlineCard.flip()
-      end
+      position={x = headlinesDeckPos.x + 3, y = headlinesDeckPos.y + 5, z = headlinesDeckPos.z},
     })
+    Wait.condition(
+      function() headlineCard.setRotationSmooth({0, 180, 0}) end,
+      || not headlineCard.isSmoothMoving()
+    )
   end
 
   if headlinesDeck.remainder ~= nill then
